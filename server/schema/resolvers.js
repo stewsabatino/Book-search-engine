0c6a561bea8e407aba2783118694933c76261631
Stew Sabatino <stewsabatino@gmail.com>
2021-12-07 09:57:46 -0600

    resolver.js queries / login and create user mutation

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,3 +1,48 @@
-const { gql } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
-const typeDefs.js
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                    .populate('books')
+                
+                return userData;
+            }
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                
+            }
+        }
+    }
+}
